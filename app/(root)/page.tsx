da8400616d6dfda87e89d5d697ca379318480e2f
sevda <sevda.m.anefi@gmail.com>
2024-08-27 16:22:20 +0300

    init repo

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Editor } from '@/components/editor/Editor';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
+
+export default Home;
